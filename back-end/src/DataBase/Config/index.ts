d7db79ccd172b4656c0bf5152b039d8f296752aa
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-14 17:51:44 -0300

    define local models

--- back-end/src/DataBase/Config/index.ts
+++ back-end/src/DataBase/Config/index.ts
@@ -1,13 +1,16 @@
-import { Dialect, Options } from 'sequelize';
+import { SequelizeOptions } from 'sequelize-typescript';
 import { env } from 'process';
+import { Dialect } from 'sequelize';
+import path from 'path';
 
-const config: Options = {
+const config: SequelizeOptions = {
   dialect: env.DB_DIALECT as Dialect,
   database: env.DB_DBNAME,
   host: env.DB_HOST,
   port: Number(env.DB_PORT),
   password: env.DB_PASS,
   username: env.DB_USER,
+  models: [path.resolve(__dirname, '..', 'Models', '**', '*.ts')],
 };
 
 export default config;
