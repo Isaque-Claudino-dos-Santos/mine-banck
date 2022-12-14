0bf9ac1ea2561fbd0e8433c4ff0ac92a437be8fb
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-14 17:52:08 -0300

    test create new client

--- back-end/src/Controllers/HomeCotroller.ts
+++ back-end/src/Controllers/HomeCotroller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
+import Client from '../DataBase/Models/Client';
 
 export default class HomeController {
   public async index(req: Request, res: Response): Promise<void> {
-    res.send('Hello Word');
+    const client = new Client({
+      name: 'isaque',
+      surname: 'santos',
+      nickname: 'isaqueMyAccess',
+      email: 'isaqueclaudino12@gmail.com',
+      password: '123',
+    });
+
+    res.send(client);
   }
 }
