39a08ad023fbb864d4cf5457812c6260e392e114
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-14 17:51:23 -0300

    sync sequelize data base

--- back-end/src/server/index.ts
+++ back-end/src/server/index.ts
@@ -1,11 +1,12 @@
 import express, { Express } from 'express';
 import { env } from 'process';
-
+import DB from '../DataBase';
 const app: Express = express();
 
 app.use(express.json());
 
 app.listen(env.APP_PORT, env.APP_HOST, () => {
+  DB.sync();
   console.log(`Open port in http://${env.APP_HOST}:${env.APP_PORT}`);
 });
 
