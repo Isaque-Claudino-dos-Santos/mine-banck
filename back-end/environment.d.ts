fbab9d4b3a4022bacf0bd0dc93419670ab58b49d
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-14 13:19:18 -0300

    add env to database

--- back-end/environment.d.ts
+++ back-end/environment.d.ts
@@ -1,9 +1,17 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      APP_PROTOCOL: 'http' | 'https';
-      APP_HOST: string | 'localhost';
+      // APP
+      APP_PROTOCOL: string;
+      APP_HOST: string;
       APP_PORT: number;
+
+      //Data Base
+      DB_DIALECT: string | 'mysql';
+      DB_PORT: number;
+      DB_HOST: string;
+      DB_USER: string;
+      DB_PASS: string;
     }
   }
 }
