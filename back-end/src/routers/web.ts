870059fce90541739a412a8bdf31648272b2b185
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-15 15:42:40 -0300

    remove controller

--- back-end/src/routers/web.ts
+++ back-end/src/routers/web.ts
@@ -1,15 +1,11 @@
 import { Router } from 'express';
 import ClientController from '../Controllers/ClientController';
-import AccountController from '../Controllers/AccountController';
 
 const router = Router();
 
 const clientController = new ClientController();
-const accountController = new AccountController();
 
 router.get('/client', clientController.index);
 router.post('/client/store', clientController.store);
-
-router.post('/client/:id/account/store', accountController.store);
 
 export default router;
