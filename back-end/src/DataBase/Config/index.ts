3cead98c8d9cbb99e5d792aedfe5d1bc061a02c6
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-17 12:11:58 -0300

    use sequelize

--- back-end/src/DataBase/Config/index.ts
+++ back-end/src/DataBase/Config/index.ts
@@ -1,16 +1,14 @@
-import { SequelizeOptions } from 'sequelize-typescript';
+import { Options } from 'sequelize';
 import { env } from 'process';
 import { Dialect } from 'sequelize';
-import path from 'path';
 
-const config: SequelizeOptions = {
+const config: Options = {
   dialect: env.DB_DIALECT as Dialect,
   database: env.DB_DBNAME,
   host: env.DB_HOST,
   port: Number(env.DB_PORT),
   password: env.DB_PASS,
   username: env.DB_USER,
-  models: [path.resolve(__dirname, '..', 'Models', '**', '*.ts')],
 };
 
 export default config;
--- back-end/src/DataBase/conection.ts
+++ back-end/src/DataBase/conection.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize-typescript';
+import { Sequelize } from 'sequelize';
 import config from './Config';
 
 const sequelize = new Sequelize(config);
