65713be5d170c534e7e6240506f14e9aa5617b9a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-16 23:39:02 -0300

    create router client acount create

--- back-end/src/routers/web.ts
+++ back-end/src/routers/web.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import ClientController from '../Controllers/ClientController';
+import AccountController from '../Controllers/AccountController';
 
 const router = Router();
 
 const clientController = new ClientController();
+const accountController = new AccountController();
 
 router.get('/client', clientController.index);
 router.post('/client/store', clientController.store);
+
+router.post('/client/:client_id/account/store', accountController.store);
 
 export default router;
