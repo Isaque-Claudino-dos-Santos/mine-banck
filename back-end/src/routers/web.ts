42477e6d1ff4f4f4904ad5fd086204897092c943
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-17 00:12:27 -0300

    create router index account

--- back-end/src/routers/web.ts
+++ back-end/src/routers/web.ts
@@ -10,6 +10,7 @@ const accountController = new AccountController();
 router.get('/client', clientController.index);
 router.post('/client/store', clientController.store);
 
+router.get('/client/:client_id/account', accountController.index);
 router.post('/client/:client_id/account/store', accountController.store);
 
 export default router;
