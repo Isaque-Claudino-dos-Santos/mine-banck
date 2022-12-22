f4939a00c36d35c82f8ad4f3929762a9d556259f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 17:54:23 -0300

    set migration in class DB

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import { dataBaseConfig, serverConfig } from './config'
-
 import DB from './DB'
 import Server from './Server'
-
 import router from './router'
+//Migrations
+import ClientsMigration from './Migrations/ClientsMigration'
 
 const db = new DB(dataBaseConfig)
+db.migrations = [ClientsMigration]
+
 const server = new Server(serverConfig.port, serverConfig.host, router)
