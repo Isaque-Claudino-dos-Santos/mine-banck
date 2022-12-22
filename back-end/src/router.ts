c12edc9bd52bbb912bf4b5d6bb761a600eb24388
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 16:55:40 -0300

    md msg

--- back-end/src/router.ts
+++ back-end/src/router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import QueryMysql from './QueryMysql/index'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send('Hello Word')
+  res.send('Welcome to home page')
 })
 
 export default router
