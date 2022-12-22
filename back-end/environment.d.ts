f3b811c852ffbec8bb5d1ff9ef84ede0afa1db29
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 15:17:11 -0300

    ajust write

--- back-end/environment.d.ts
+++ back-end/environment.d.ts
@@ -1,19 +1,19 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: string;
+      NODE_ENV: string
 
-      APP_PROTOCOL: string;
-      APP_HOST: string;
-      APP_PORT: string;
+      APP_PROTOCOL: string
+      APP_HOST: string
+      APP_PORT: string
 
-      DB_DIALECT: string;
-      DB_PORT: string;
-      DB_HOST: string;
-      DB_USER: string;
-      DB_PASS: string;
-      DB_DBNAME: string;
+      DB_DIALECT: string
+      DB_PORT: string
+      DB_HOST: string
+      DB_USER: string
+      DB_PASS: string
+      DB_DBNAME: string
     }
   }
 }
-export {};
+export {}
