16489ee3e9c53c9faceca1513c8eb34a357f3a98
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 18:41:07 -0300

    ajust unic prop connection

--- back-end/src/DB/DB.ts
+++ back-end/src/DB/DB.ts
@@ -1,15 +1,20 @@
 import ConnectionDB from './ConnectionDB'
 import { Connection, ConnectionOptions } from 'mysql2'
 import Migration from './Migration'
+import MigrationHandler from './MigrationHandler'
 
 export default class DB {
-  private readonly connectionDB: ConnectionDB
-
   public readonly connection: Connection
-  public migrations: typeof Migration[] = []
+  public readonly migration: MigrationHandler
 
-  constructor(connectionOptions: ConnectionOptions) {
-    this.connectionDB = new ConnectionDB(connectionOptions)
-    this.connection = this.connectionDB.onConnect()
+  constructor(
+    connectionOptions: ConnectionOptions,
+    private migrationsInstances: Migration[] = []
+  ) {
+    this.connection = new ConnectionDB(connectionOptions).onConnect()
+    this.migration = new MigrationHandler(
+      this.connection,
+      this.migrationsInstances
+    )
   }
 }
