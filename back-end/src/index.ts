564ba926cc61b4fb992459e2628f1fbc5c9c08bc
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 15:59:30 -0300

    add router in server

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,5 +1,11 @@
 import 'dotenv/config'
+
 import DB from './DB'
-import { dataBaseConfig } from './config'
+import Server from './server'
+
+import { dataBaseConfig, serverConfig } from './config'
+
+import router from './router'
 
 const db = new DB(dataBaseConfig)
+const server = new Server(serverConfig.port, serverConfig.host, router)
