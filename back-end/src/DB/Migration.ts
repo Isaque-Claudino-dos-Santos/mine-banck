990940f742a5e6e5fb6c4f5727ac7c994780c067
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 18:41:15 -0300

    add abstract prop name

--- back-end/src/DB/Migration.ts
+++ back-end/src/DB/Migration.ts
@@ -3,6 +3,8 @@ import QueryTable from '../QueryMysql/QueryTable'
 export default abstract class Migration {
   protected readonly table = new QueryTable()
 
+  abstract name: string
+
   abstract up(): string
 
   abstract down(): string
