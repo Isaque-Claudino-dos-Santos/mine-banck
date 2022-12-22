81f02c980ba2ec36b4d239f86b2a5fd1e468493f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 20:26:07 -0300

    add migration account

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -5,7 +5,14 @@ import Server from './Server'
 import router from './router'
 //Migrations
 import ClientsMigration from './Migrations/ClientsMigration'
+import AccountMigration from './Migrations/AccountsMigration'
 
-const db = new DB(dataBaseConfig, [new ClientsMigration()])
+const db = new DB(dataBaseConfig, [
+  new ClientsMigration(),
+  new AccountMigration(),
+])
 
 const server = new Server(serverConfig.port, serverConfig.host, router)
+
+// db.migration.upAll()
+// db.migration.downAll()
