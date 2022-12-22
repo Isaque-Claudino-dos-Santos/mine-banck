ab842a62df1f51423fd22cf0891f91f58f5adeb6
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 20:38:28 -0300

    add on delete and on update

--- back-end/src/QueryMysql/QueryTable/QueryAssociateTable.ts
+++ back-end/src/QueryMysql/QueryTable/QueryAssociateTable.ts
@@ -1,11 +1,30 @@
+type TypeRefOptions =
+  | 'cascade'
+  | 'set null'
+  | 'restrict'
+  | 'no action'
+  | 'set default'
+
 export default class QueryAssociateTable {
   private tableRef: string = ''
   private primaryKey: string = ''
+  private update: TypeRefOptions = 'set default'
+  private delete: TypeRefOptions = 'set default'
 
   constructor(
     private readonly table: string,
     private readonly foreignKey: string
   ) {}
+
+  public onUpdate(option: TypeRefOptions): this {
+    this.update = option
+    return this
+  }
+
+  public onDelete(option: TypeRefOptions): this {
+    this.delete = option
+    return this
+  }
 
   public reference(table: string, primaryKey: string): this {
     this.tableRef = table
@@ -17,6 +36,6 @@ export default class QueryAssociateTable {
     if (this.tableRef === '' || this.primaryKey === '')
       throw console.log('Reference not passed!')
 
-    return `alter table ${this.table} add foreign key (${this.foreignKey}) references ${this.tableRef}(${this.primaryKey})`
+    return `alter table ${this.table} add foreign key (${this.foreignKey}) references ${this.tableRef}(${this.primaryKey}) on update ${this.update} on delete ${this.delete}`
   }
 }
