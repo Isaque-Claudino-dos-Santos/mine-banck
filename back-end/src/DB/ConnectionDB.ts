57207c34bd68400d88767b8f3400083fdb7d9d3a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 15:35:40 -0300

    rn method

--- back-end/src/DB/ConnectionDB.ts
+++ back-end/src/DB/ConnectionDB.ts
@@ -3,7 +3,7 @@ import mysql2 from 'mysql2'
 export default class ConnectionDB {
   constructor(private readonly options: mysql2.ConnectionOptions) {}
 
-  async connection(): Promise<mysql2.Connection> {
+  async onConnect(): Promise<mysql2.Connection> {
     return mysql2.createConnection(this.options)
   }
 }
