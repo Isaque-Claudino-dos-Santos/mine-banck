891bafadcb09ef13863e36560bc3e8a1681e0bf0
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 20:21:37 -0300

    create account table

---
+++ back-end/src/Migrations/AccountsMigration.ts
@@ -0,0 +1,27 @@
+import Migration from '../DB/Migration'
+
+export default class AccountMigration extends Migration {
+  name: string = 'account'
+
+  up(): string {
+    return this.table
+      .create(this.name)
+      .column('id', ['int', 'auto_increment', 'primary key', 'not null'])
+      .column('money', ['int', 'default(0)'])
+      .column('client_id', ['int'])
+      .build()
+  }
+
+  down(): string {
+    return this.table.drop(this.name).build()
+  }
+
+  associates(): string[] {
+    return [
+      this.table
+        .assoc(this.name, 'client_id')
+        .reference('clients', 'id')
+        .build(),
+    ]
+  }
+}
