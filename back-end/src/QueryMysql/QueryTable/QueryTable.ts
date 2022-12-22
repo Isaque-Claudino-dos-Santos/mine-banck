3a738ca21b2b657a3f4999ac3601adafe6fe6389
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-22 17:32:17 -0300

    add method drop

--- back-end/src/QueryMysql/QueryTable/QueryTable.ts
+++ back-end/src/QueryMysql/QueryTable/QueryTable.ts
@@ -1,7 +1,12 @@
 import QueryCreateTable from './QueryCreateTable'
+import QueryDropTable from './QueryDropTable/QueryDropTable'
 
 export default class QueryTable {
   public create(tableName: string): QueryCreateTable {
     return new QueryCreateTable(tableName)
+  }
+
+  public drop(tableName: string): QueryDropTable {
+    return new QueryDropTable(tableName)
   }
 }
